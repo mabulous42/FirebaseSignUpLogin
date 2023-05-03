4f8432adea22183a613cf758f2df1829a59a52e7
Abbas <tundeeey@gmail.com>
2023-05-03 18:41:49 +0100

    added a new folder for Dashboard and its respective html,js and css files

--- public/SignUp/signup.js
+++ public/SignUp/signup.js
@@ -1,13 +1,16 @@
 let userName = document.getElementById("user-name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
+firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
   .then((userCredential) => {
     // Signed in 
     var user = userCredential.user;
-    // ...
+    alert("Signed up successfully");
+    console.log(user);
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ..
+    alert(errorMessage);
   });
