702c0bb34ac66df13519a9b308a01c62d9e1aeaf
Abbas <tundeeey@gmail.com>
2023-05-03 19:09:32 +0100

    added a condition to check if the length of the passwrod is at least 8 characters long before a successful sign up is done else it will alert that the password is short

--- public/SignUp/signup.js
+++ public/SignUp/signup.js
@@ -5,16 +5,24 @@ let password = document.getElementById("password");
 
 function signUp(ev) {
     ev.preventDefault();
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-      .then((userCredential) => {
-        // Signed in 
-        var user = userCredential.user;
-        alert("Signed up successfully");
-        console.log(user);
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-      });
+    if (password.value.length < 8) {
+        alert("Password must be atleast 8 characters long")
+    } else {
+        
+        firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                alert("Signed up successfully");
+                userName.value = "";
+                email.value = "";
+                password.value = "";
+                console.log(user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                alert(errorMessage);
+            });
+    }
 }
