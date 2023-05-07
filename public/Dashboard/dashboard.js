c7a3f707ff0166c37af7b2499f4adbafaabbea00
Abbas <tundeeey@gmail.com>
2023-05-07 01:33:30 +0100

    added an id to my object data so that i can use the id to keep track of the data if it actually exist in my database

--- public/Dashboard/dashboard.js
+++ public/Dashboard/dashboard.js
@@ -1,9 +1,8 @@
-let userName = document.getElementById("userName");
+let userName = document.getElementById("profile-name");
 let postModal = document.getElementById("myPostModal");
-let postBtn = document.getElementById("post-btn");
-let content = document.getElementById("content");
 let showPostTag = document.getElementById("show-post");
 let writeAPostTag = document.getElementById("open-post-modal");
+let whatsOnYourMindTag = document.getElementById("on-your-mind");
 
 postModal.style.visibility = "hidden";
 
@@ -11,30 +10,12 @@ let currentUser = JSON.parse(localStorage.getItem("CU"));
 console.log(currentUser);
 
 console.log(currentUser.name);
+userName.innerHTML = currentUser.name;
 // firebase.auth().signOut().then(() => {
 //     // Sign-out successful.
 //   }).catch((error) => {
 //     // An error happened.
 //   });
-
-// function postBlog(ev) {
-//     let data = {
-//         author: author.value,
-//         title: title.value,
-//         content: content.value
-//     }
-//     console.log(data);
-//     ev.preventDefault();
-//     // Add a new document in collection "cities"
-//     db.collection("tweets").doc().set(data)
-//         .then(() => {
-//             console.log("Document successfully written!");
-//             window.location.href = "blog.html";
-//         })
-//         .catch((error) => {
-//             console.error("Error writing document: ", error);
-//         });
-// }
 
 
 function writeSomething() {
@@ -89,14 +70,19 @@ function closePostModal() {
     postModal.style.visibility = "hidden";
 }
 
+let postBtn = document.getElementById("post-btn");
 function enableButton() {
     postBtn.disabled = false;
 }
 
+let content = document.getElementById("content");
+let num = Number(1);
 function createPost() {
     let data = {
+        id: num,
         author: currentUser.name,
-        content: content.value
+        content: content.value,
+        isLike: false
     }
 
     // Add a new document in collection "cities"
@@ -104,9 +90,10 @@ function createPost() {
         .then(() => {
             console.log("Document successfully written!");
             // window.location.href = "blog.html";
-
+            
             content.value = "";
             postModal.style.visibility = "hidden";
+            num++;
             displayAllPost();
 
         })
@@ -115,71 +102,77 @@ function createPost() {
         });
 }
 
+let ref;
 function displayAllPost() {
     showPostTag.innerHTML = "";
     db.collection("Feeds").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc,index) => {
+        querySnapshot.forEach((doc, index) => {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
+            console.log(doc.data().isLike);
+            
             showPostTag.innerHTML += `
             <div class="rounded w-100 mb-3 p-3 my-post-div">
-                        <div class="d-flex align-items-center pix-div">
-                            <h6
-                                class="me-2 text-black rounded-circle bg-white d-flex align-items-center justify-content-center pix">
-                                A</h6>
-                            <h6 class="">${currentUser.name} ${doc.id}</h6>
+                <div class="d-flex align-items-center pix-div">
+                    <h6
+                        class="me-2 text-black rounded-circle bg-white d-flex align-items-center justify-content-center pix">
+                        A</h6>
+                    <h6 class="">${currentUser.name}</h6>
+                </div>
+                <div class="w-100">
+                    <p>${doc.data().content}</p>
+                </div>
+                <div class="w-100">
+                    <img src="../images/photo.webp" alt="" class="w-100">
+                </div>
+                <hr class="mb-1">
+                <div class="d-flex align-items-center justify-content-center">
+                    <button id="likeBtn" class="like rounded w-100 text-center text-center d-flex align-items-center justify-content-center p-2" onclick="isLike(${doc.data().id})">
+                        <div class="me-2">
+                            <i class="fa-solid fa-thumbs-up like-icon"></i>
                         </div>
-                        <div class="w-100">
-                            <p>${doc.data().content}</p>
+                        <div class="like1-text">Like</div>
+                    </button>
+                    <button class="comment rounded w-100 text-center d-flex align-items-center justify-content-center p-2">
+                        <div class="me-2">
+                            <i class="fa-sharp fa-solid fa-comment comment-icon"></i>
                         </div>
-                        <div class="w-100">
-                            <img src="../images/photo.webp" alt="" class="w-100">
-                        </div>
-                        <hr class="mb-1">
-                        <div class="d-flex align-items-center justify-content-center">
-                            <button class="like rounded w-100 text-center text-center d-flex align-items-center justify-content-center p-2" onclick="isLike(${doc.id})">
-                                <div class="me-2">
-                                    <i class="fa-solid fa-thumbs-up like-icon"></i>
-                                </div>
-                                <div class="like1-text">Like</div>
-                            </button>
-                            <button class="comment rounded w-100 text-center d-flex align-items-center justify-content-center p-2">
-                                <div class="me-2">
-                                    <i class="fa-sharp fa-solid fa-comment comment-icon"></i>
-                                </div>
-                                <div class="comment1-text">Comment</div>
-                            </button>
-                            <button class="share rounded w-100 text-center d-flex align-items-center justify-content-center p-2">
-                                <div class="me-2"><i class="fa-solid fa-share share-icon"></i></div>
-                                <div class="share1-text">Share</div>
-                            </button>
-                        </div>
-                        <hr class="mt-1">
-                        <div class="d-flex align-items-center">
-                            <div class="pics-div me-2">
-                                <h6 class="pics bg-white text-black rounded-circle d-flex align-items-center justify-content-center">B</h6>
-                            </div>
-                            <div class="w-100 position-relative">
-                                <input type="text" name="" id="" placeholder="Write a comment..." class="comment-input p-1 ps-3 rounded-pill w-100">
-                                <button class="position-absolute comment-btn d-flex align-items-center justify-content-center rounded-circle">
-                                <i class="fa-solid fa-square-arrow-up-right send-comment"></i>
-                                </button>
-                            </div>
-                        </div>
+                        <div class="comment1-text">Comment</div>
+                    </button>
+                    <button class="share rounded w-100 text-center d-flex align-items-center justify-content-center p-2">
+                        <div class="me-2"><i class="fa-solid fa-share share-icon"></i></div>
+                        <div class="share1-text">Share</div>
+                    </button>
+                </div>
+                <hr class="mt-1">
+                <div class="d-flex align-items-center">
+                    <div class="pics-div me-2">
+                        <h6 class="pics bg-white text-black rounded-circle d-flex align-items-center justify-content-center">B</h6>
                     </div>
+                    <div class="w-100 position-relative">
+                        <input type="text" name="" id="" placeholder="Write a comment..." class="comment-input p-1 ps-3 rounded-pill w-100">
+                        <button class="position-absolute comment-btn d-flex align-items-center justify-content-center rounded-circle">
+                        <i class="fa-solid fa-square-arrow-up-right send-comment"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
             `
         });
     });
 }
 displayAllPost();
 
+let likedBtn = document.getElementById("likeBtn")
 function isLike(id) {
-    // db.collection("Feeds").get().then((querySnapshot) => {
-    //     querySnapshot.forEach((doc,index) => {
-    //         // doc.data() is never undefined for query doc snapshots
-    //         console.log(doc.id, " => ", doc.data());
-    //     })
-    // })
-    // let liked = 
-    console.log(id);
+    db.collection("Feeds").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc,index) => {
+            // doc.data() is never undefined for query doc snapshots
+            let liked = doc.data().id == id;
+            if (liked == true) {
+
+                console.log(liked);
+            }
+        })
+    })
 }
